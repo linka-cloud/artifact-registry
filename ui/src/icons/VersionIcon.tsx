1d828011f568c46a9fc77271e3eaa14bdc121453
Adphi <philippe.adrien.nousse@gmail.com>
2023-10-11 18:43:06 +0200

    web: fix version icon hidden in dark mode
    Signed-off-by: Adphi <philippe.adrien.nousse@gmail.com>

--- ui/src/icons/VersionIcon.tsx
+++ ui/src/icons/VersionIcon.tsx
@@ -12,10 +12,17 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { SvgIcon, SvgIconProps } from '@mui/material'
+import { SvgIcon, SvgIconProps, useTheme } from '@mui/material'
 
-export const VersionIcon = (props: SvgIconProps) => (
-  <SvgIcon {...props} viewBox="0 0 21 21">
-    <g fill="none" fillRule="evenodd" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" transform="translate(2 4)"> <path d="m.5 8.5 8 4 8.017-4"></path> <path d="m.5 4.657 8.008 3.843 8.009-3.843-8.009-4.157z"></path> </g>
-  </SvgIcon>
-)
+export const VersionIcon = (props: SvgIconProps) => {
+  const theme = useTheme()
+  return (
+    <SvgIcon {...props} viewBox='0 0 21 21'>
+      <g fill='none' fillRule='evenodd' stroke={theme.palette.text.primary}
+         strokeLinecap='round' strokeLinejoin='round' transform='translate(2 4)'>
+        <path d='m.5 8.5 8 4 8.017-4'></path>
+        <path d='m.5 4.657 8.008 3.843 8.009-3.843-8.009-4.157z'></path>
+      </g>
+    </SvgIcon>
+  )
+}
